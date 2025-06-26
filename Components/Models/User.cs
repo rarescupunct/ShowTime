0b84b423fae02a88e74fe97ceaa124bd962eb4d2
rarescupunct <fira.rares4049@gmail.com>
2025-06-26 15:29:40 +0300

    first commit v2

---
+++ Components/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace ShowTime.Components.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string Username { get; set; }
+}
